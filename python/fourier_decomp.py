0c9d3ec2a507db9459fc06c764ced4a5a534add8
camille chaize <c.chaize74@gmail.com>
2022-04-02 13:40:24 +0200

    version python - mean, coefficients correction

--- python/fourier_decomp.py
+++ python/fourier_decomp.py
@@ -6,10 +6,10 @@ import math
 # Function To Sample
 period = 1
 pulsation = 2 * math.pi / period
-d = 0.2  # cyclic
+d = 0.7  # cyclic
 
 # Precision
-n = 20  # decomposition precision
+n = 50  # decomposition precision
 p = 500  # graph precision
 number_periods = 2
 
@@ -22,14 +22,22 @@ def periodic_function(t):
         return 0
 
 
+def periodic_function_2(t):
+    if (t % period) / period < d:
+        return math.cos(pulsation*t)
+    else:
+        return -math.cos(pulsation*t)
+
+
 def coefficients_calculation(function_to_sample):
+    m_0 = integral.quad(lambda x: function_to_sample(x), 0, period)[0]
     a_n = np.empty(n)
     b_n = np.empty(n)
-    for k in range(n):
-        a_n[k] = (integral.quad(lambda x: function_to_sample(x) * math.cos(k * pulsation * x), 0, period))[0]
-        b_n[k] = (integral.quad(lambda x: function_to_sample(x) * math.sin(k * pulsation * x), 0, period))[0]
+    for k in range(1, n):
+        a_n[k] = (2/period)*(integral.quad(lambda x: function_to_sample(x) * math.cos(k * pulsation * x), 0, period))[0]
+        b_n[k] = (2/period)*(integral.quad(lambda x: function_to_sample(x) * math.sin(k * pulsation * x), 0, period))[0]
 
-    return a_n, b_n
+    return m_0, a_n, b_n
 
 
 def fourier_series_decomposition(function_to_sample):
@@ -40,9 +48,9 @@ def fourier_series_decomposition(function_to_sample):
     y_values = [0.0] * p
 
     for i, x in enumerate(x_values):
-        y = 0.0
-        for k in range(x_n[0].shape[0]):
-            y += x_n[0][k] * math.cos(k * pulsation * x) + x_n[1][k] * math.sin(k * pulsation * x)
+        y = x_n[0]
+        for k in range(x_n[1].shape[0]):
+            y += x_n[1][k] * math.cos(k * pulsation * x) + x_n[2][k] * math.sin(k * pulsation * x)
         y_values[i] = y
 
     return x_values, y_values
@@ -51,8 +59,8 @@ def fourier_series_decomposition(function_to_sample):
 def plot_estimation(function_to_sample):
     x_values, y_values = fourier_series_decomposition(function_to_sample)
     x_ref, y_ref = [number_periods * period * t / p for t in range(0, p, 1)], [function_to_sample(number_periods * period * t / p) for t in range(0, p, 1)]
-    plt.plot(x_values, y_values, "g")
-    plt.plot(x_ref, y_ref, "b")
+    plt.plot(x_values, y_values, "r")
+    plt.plot(x_ref, y_ref, "g")
     plt.show()
 
 
